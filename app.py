3008ba991fde9fa7688b9ea1cc7d6714916778f5
Jesús Petro <85887171+JesusPetro@users.noreply.github.com>
2022-04-24 12:24:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ from logic.identificarPlaca import idPlaca
 
 app = Flask(__name__)
 
-app.config['uploadFolder'] = "./static/images"
+app.config['uploadFolder'] = "./static/image"
 app.config['MYSQL_HOST'] = 'localhost' 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'registro_acceso_vehicular' #
+app.config['MYSQL_USER'] = 'root' #user de la base
+app.config['MYSQL_PASSWORD'] = 'J08042005' #contraseña de la base
+app.config['MYSQL_DB'] = 'registro_acceso_vehicular' #nombre de la base 
 mysql = MySQL(app)
 
 # settings
@@ -20,9 +20,13 @@ app.secret_key = "mysecretkey"
 
 # pagina de inicio
 @app.route("/")
-def index_visitante():
-    return render_template('index_visitante.html')
+def Informacion():
+    return render_template('Informacion.html')
 
+
+@app.route("/index_visitante")
+def index_visitante():
+    return render_template("index_visitante.html")
 
 # ------------ VISITANTES REGISTRO  ---------------------------
 
@@ -78,13 +82,15 @@ def loguin_vigilante():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM vigilantes')
     vigilantes = cur.fetchall()
+    cur.execute('SELECT * FROM  visitantes')
+    visitantes =  cur.fetchall()
     for vig in vigilantes:
         if vig[3]==email and vig[4]==password:
             cur.execute('SELECT * FROM visitas')
             visitas_rej = cur.fetchall()
             global vigilante_sesion
             vigilante_sesion=vig
-            return render_template('home_vigilante.html', vigilante=vig, visitas=visitas_rej)
+            return render_template('home_vigilante.html', vigilante=vig, visitas=visitas_rej, visitantes= visitantes)
     
     flash(" usuario ingresado es incorrecto ")
     return render_template('index_vigilante.html')
@@ -97,8 +103,10 @@ def home_vigilante():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM visitas')
     visitas_rej = cur.fetchall()
+    cur.execute('SELECT * FROM  visitantes')
+    visitantes =  cur.fetchall()
     mysql.connection.commit()
-    return render_template('home_vigilante.html', vigilante=vigilante_sesion, visitas=visitas_rej)
+    return render_template('home_vigilante.html', vigilante=vigilante_sesion, visitas=visitas_rej,visitantes= visitantes)
 
 
 @app.route("/uploader",methods=['POST'])
@@ -118,6 +126,7 @@ def validar_visita():
     global vigilante_sesion
     
     placa= request.form['placa']
+    _Placa = placa.rstrip()
 
     # obtenemos fecha yn hora del registro de la visita 
     now = datetime.now()
@@ -127,22 +136,38 @@ def validar_visita():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM visitantes')
     visitantes = cur.fetchall()
+    print(visitantes)
+
     for vist in visitantes:
-        if placa==vist[5]:
-            cur.execute("INSERT INTO visitas (nombre_visitante, apellido_visitante, placa_vehiculo, fecha, hora_entrada, estado ) VALUES (%s,%s,%s,%s,%s,%s)",( vist[1],vist[2],vist[5], dia, h_entrada,1))
-            mysql.connection.commit()
-            cur.execute('SELECT * FROM visitas')
-            visitas_rej = cur.fetchall()
-            mysql.connection.commit()
-            flash("visita registrada exitosamente")
-            return render_template('home_vigilante.html',visitas=visitas_rej,mensaje_error=0, vigilante=vigilante_sesion)
-    
+        print (vist )
+        if _Placa==vist[5]:
+            if vist[7] == 0:
+                cur.execute("UPDATE visitantes SET activo = 1")
+                cur.execute("INSERT INTO visitas (nombre_visitante, apellido_visitante, placa_vehiculo, fecha, hora_entrada, estado ) VALUES (%s,%s,%s,%s,%s,%s)",( vist[1],vist[2],vist[5], dia, h_entrada,1))
+                mysql.connection.commit()
+                cur.execute('SELECT * FROM visitas')
+                visitas_rej = cur.fetchall()
+                mysql.connection.commit()
+                flash("visita registrada exitosamente")
+                return render_template('home_vigilante.html',visitas=visitas_rej, visitantes= visitantes,  mensaje_error=0, vigilante=vigilante_sesion)
+
+            else: 
+                cur.execute("UPDATE visitantes SET activo = 0")
+                sentencia = "DELETE FROM visitas WHERE placa_vehiculo LIKE ('{0}')".format (_Placa)
+                cur.execute (sentencia)
+                mysql.connection.commit()
+                cur.execute('SELECT * FROM visitas')
+                visitas_rej = cur.fetchall()
+                mysql.connection.commit()
+                flash("El vehiculo ha salido, que tenga buen viaje.")
+                return render_template('home_vigilante.html',visitas=visitas_rej, visitantes= visitantes, mensaje_error=0, vigilante=vigilante_sesion)
+
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM visitas')
     visitas_rej = cur.fetchall()
     mysql.connection.commit()
     flash(" La placa ingresada no se encuentra en la base de datos de visitantes porfavor verificar ")
-    return render_template('home_vigilante.html',visitas=visitas_rej, mensaje_error=1, vigilante=vigilante_sesion)
+    return render_template('home_vigilante.html',visitas=visitas_rej,visitantes= visitantes, mensaje_error=1, vigilante=vigilante_sesion)
     
 
 if __name__ == '__main__':
